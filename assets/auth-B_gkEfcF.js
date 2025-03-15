78a60477c5a69ba8fc47f0e1e811853befc91314
rey.li <rey.li@lalamove.com>
2025-03-15 10:44:55 +0800

    Updates

--- assets/auth-B_gkEfcF.js
+++
@@ -1,1 +0,0 @@
-import"./index-gwXPppyA.js";const a=o=>({admin:"管理员",editor:"编辑者",guest:"访客"})[o]||"未知角色",r=o=>({admin:"#1890ff",editor:"#52c41a",guest:"#faad14"})[o]||"#d9d9d9";export{r as a,a as g};
